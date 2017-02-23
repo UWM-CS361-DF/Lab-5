28c1b8477eb4cbede92169c0e8bc3b2c00de2b7b
PupilDark <sdong@uwm.edu>
2017-02-22 23:48:37 -0600

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -6,16 +6,15 @@ public class Bank {
 		accounts.add(new Account(1234, 6789, 80));
 		accounts.add(new Account(6789, 4321, 60));
 	}
-	static Account a;
-	public int validate(int account) throws Exception{
+	Account acct;
+	public Account validate(int account) throws Exception{
 		int i = 0;
-		this.a = accounts.get(i);
-		while(a.accountNumber != account){
+		while(acct.accountNumber != account){
 			i++;
-			this.a = accounts.get(i);
+			acct = accounts.get(i);
 			if(i >= accounts.size())
 				throw new Exception("Error");
 		}
-		return a.accountNumber;
+		return acct;
 	}
 }
