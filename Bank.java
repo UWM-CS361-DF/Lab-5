97757d718c968e7709cd503b140fd3aabd06361a
PupilDark <sdong@uwm.edu>
2017-02-22 23:36:18 -0600

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,20 +1,21 @@
 import java.util.*;
 public class Bank {
 	static List<Account> accounts;
-	public int account;
 	public Bank(){
 		accounts = new ArrayList<Account>();
 		accounts.add(new Account(1234, 6789, 80));
 		accounts.add(new Account(6789, 4321, 60));
 	}
-	
+	static Account a;
 	public int validate(int account) throws Exception{
 		int i = 0;
-		while(accounts.get(i).accountNumber != account){
+		this.a = accounts.get(i);
+		while(a.accountNumber != account){
 			i++;
+			this.a = accounts.get(i);
 			if(i >= accounts.size())
 				throw new Exception("Error");
 		}
-		return accounts.get(i).accountNumber;
+		return a.accountNumber;
 	}
 }
