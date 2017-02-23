04c81afae00f1cc3f6df4b09689c816e58653314
PupilDark <sdong@uwm.edu>
2017-02-22 22:38:12 -0600

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -1,13 +1,24 @@
 import java.util.*;
 
 public class ATM {
-
+	CardReader cr = new CardReader();
+	Printer pt = new Printer();
+	CashDispenser cash = new CashDispenser();
+	Bank bank = new Bank();
+	int valid;
+	static int pin;
+	int bal;
+	Account acct = new Account(valid, pin, bal); 
+	
 	public void start() throws Exception{
-		int account = CardReader.acctNumber();
-		Bank bank = new Bank();
-		Account valid = bank.validate(account);
-		if(valid.validate()){
+		int account = cr.acctNumber();
+		valid = bank.validate(account);
+		System.out.println("Enter PIN: ");
+		Scanner stdIn = new Scanner(System.in);
+		pin = stdIn.nextInt();
+		if(acct.validate()){
 			
 		}
 	}
+
 }
